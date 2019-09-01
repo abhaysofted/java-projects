87e4f3557c3affcf6212dd1701bb11d38dcc82d9
abhaysofted <abhaygupta71@gmail.com>
2019-09-01 09:02:32 +0530

    added java concepts

---
+++ java-concepts/src/com/softed/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.softed.singleton;
+
+public class Singleton {
+
+	private Singleton singleton;
+	
+	private Singleton() {}
+	
+	public Singleton getInstance() {
+		
+		if(singleton == null) {
+			singleton = new Singleton();
+		}
+		return singleton;
+	}
+}
